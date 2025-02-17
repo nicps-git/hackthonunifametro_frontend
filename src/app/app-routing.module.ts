baa098a926546afca820cfd511c48192c4b6676a
Caio Orleans <caioorleansdm@gmail.com>
2025-02-17 20:09:41 -0300

    Estrutura inicial da página

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./feature/auth/auth.module').then(m => m.AuthModule) }];
+const routes: Routes = [
+  { 
+    path: 'auth', 
+    loadChildren: () => import('./feature/auth/auth.module').then(m => m.AuthModule) 
+  },
+  {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
