15ad1f4fdcc87d46896a8a0887cd70cd365a6677
Caio Orleans <caioorleansdm@gmail.com>
2025-02-21 16:09:34 -0300

    Button with loading component

--- src/app/feature/auth/auth.module.ts
+++ src/app/feature/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -8,7 +7,6 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { PasswordRecoveryComponent } from './pages/password-recovery/password-recovery.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { PasswordRedefineComponent } from './pages/password-redefine/password-redefine.component';
@@ -28,7 +26,6 @@ import { SharedModule } from 'src/app/shared/shared.module';
     MatInputModule, 
     MatIconModule,
     MatButtonModule,
-    MatProgressSpinnerModule,
     MatSnackBarModule,
     SharedModule
   ]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+
 import { FrameComponent } from './components/frame/frame.component';
+import { ButtonWithLoadingComponent } from './components/button-with-loading/button-with-loading.component';
 
 
 
 @NgModule({
   declarations: [
     FrameComponent,
+    ButtonWithLoadingComponent,
   ],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
   ],
   exports: [
-    FrameComponent
+    FrameComponent,
+    ButtonWithLoadingComponent,
   ]
 })
 export class SharedModule { }
